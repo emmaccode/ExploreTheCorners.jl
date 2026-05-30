4896caef26bacf7b2b2d03e5771026a9d8187141
emmy <52672675+emmaccode@users.noreply.github.com>
2026-05-30 00:04:54 -0600

    significant update, more locations, menu

--- src/ExploreTheCorners.jl
+++ src/ExploreTheCorners.jl
@@ -2,6 +2,7 @@ module ExploreTheCorners
 using Toolips
 using Toolips.Components
 using ToolipsSession
+import Toolips: on_start
 
 abstract type AbstractLocation end
 
@@ -9,46 +10,58 @@ mutable struct SubLocations <: AbstractLocation
 
 end
 
-approx_offset = (165, 175)
+approx_offset = (0, 0)
 
 mutable struct Location <: AbstractLocation
     name::String
     position::Tuple{Int64, Int64}
-    cover::String
+    cover::Bool
     description::Component{:div}
 end
 
 locations = Vector{Location}()
 
-for dir_file in readdir("public/locations")
-    if dir_file == "backgrounds"
-        continue
+function load_locations!(locations::Vector{Location})
+    for dir_file in readdir("public/locations")
+        if dir_file == "backgrounds"
+            continue
+        end
+        full_path::String = "public/locations/" * dir_file
+        if isfile(full_path)
+            continue
+        end
+        name_splits = split(dir_file, "SEP")
+        cover = false
+        if isfile(full_path * "/images/cover.jpg")
+            cover = true
+        end
+        descmd = div("null")
+        if isfile(full_path * "/description.md")
+            descmd = Components.tmd("description", read(full_path * "/description.md", String))
+            descmd[:align] = "left"
+        end
+        dim = (parse(Int64, name_splits[2]), parse(Int64, name_splits[3]))
+        push!(locations, 
+            Location(string(replace(name_splits[1], "-" => " ")), dim,
+            cover, descmd))
     end
-    full_path::String = "public/locations/" * dir_file
-    if isfile(full_path)
-        continue
-    end
-    name_splits = split(dir_file, "SEP")
-    cover = ""
-    if isfile(full_path * "/cover.jpg")
-        cover = "locations/$dir_file" * "/cover.jpg"
-    end
-    descmd = div("null")
-    if isfile(full_path * "/description.md")
-        descmd = Components.tmd("description", read(full_path * "/description.md", String))
-    end
-    dim = (parse(Int64, name_splits[2]), parse(Int64, name_splits[3]))
-    push!(locations, 
-        Location(string(replace(name_splits[1], "-" => " ")), dim,
-        cover, descmd))
 end
 
+get_filepath(location::Location) = begin
+    "locations/$(location.name)SEP$(location.position[1])SEP$(location.position[2])"
+end
 #==
 extensions
 ==#
 logger = Toolips.Logger()
 SESSION = ToolipsSession.Session()
 default_404 = Toolips.default_404
+
+LOAD_LOCATIONS = Toolips.QuickExtension{:locs}()
+
+function on_start(ext::Toolips.QuickExtension{:locs}, data::Dict{Symbol, Any}, rs::Vector{<:Toolips.AbstractHTTPRoute})
+    load_locations!(ExploreTheCorners.locations)
+end
 
 #==
 routes
@@ -94,10 +107,12 @@ end
 menu_box = begin
     men_pairs = ("flora" => "/images/icons/menu/plants.png", "fauna" => "/images/icons/menu/animals.png", "entomofauna" => "/images/icons/menu/bugs.png", 
     "geology" => "/images/icons/menu/geology.png", "anthropology" => "/images/icons/menu/anthro.png", 
-    "archaeology" => "/images/icons/menu/arche.png", "about" => "/images/icons/menu/about.png")
+    "archaeology" => "/images/icons/menu/arche.png", "contemporary pueblo" => "/images/icons/menu/contemporary.png", 
+    "about" => "/images/icons/menu/about.png")
     mens = [begin 
         name, url = menpair[1], menpair[2]
-        new_men = div("$(name)men", class = "menuitem", align = "center", 
+        safename = replace(name, " " => "")
+        new_men = div("$(safename)men", class = "menuitem", align = "center", 
         children = [img(src = url, width = 50)])
         on(new_men, "mouseenter") do cl::ClientModifier
             set_text!(cl, new_men, name)
@@ -105,11 +120,17 @@ menu_box = begin
         on(new_men, "mouseleave") do cl::ClientModifier
             set_children!(cl, new_men, [img(src = url, width = 50)])
         end
+        on(new_men, "click") do cl::ClientModifier
+            style!(cl, "menubox", "opacity" => 0percent)
+            next!(cl, "menubox") do cl2::ClientModifier
+                redirect!(cl2, "/$name")
+            end
+        end
         new_men
     end for menpair in men_pairs]
     men_box = div("menubox", children = mens)
     style!(men_box, "position" => "absolute", "width" => 40percent, "left" => 30percent, "top" => 20percent, "grid-template-columns" => "1fr 1fr 1fr 1fr", 
-    "display" => "grid", "animation-name" => "fadeup", "animation-duration" => 850ms, "z-index" => 19, "background" => "transparent")
+    "display" => "grid", "animation-name" => "fadeup", "animation-duration" => 850ms, "z-index" => 19, "background" => "transparent", "transition" => 500ms)
     men_box
 end
 
@@ -174,7 +195,7 @@ image_options = readdir("public/locations/backgrounds")
 main = route("/") do c::Toolips.AbstractConnection
     write!(c, stylesheet)
     main_curs = Components.cursor("maincurs")
-    main_svg = svg(width = 1000pt, height = 100percent, text = read("public/images/maps/corners.svg", String))
+    main_svg = svg(width = 100percent, height = 100percent, text = read("public/images/maps/corners.svg", String))
     style!(main_svg, "overflow" => "scroll")
     for location in locations
         safename = replace(location.name, " " => "")
@@ -185,8 +206,9 @@ main = route("/") do c::Toolips.AbstractConnection
             if ~("$safename-label"  in cm)
                 new_label = div("$safename-label", class = "hoverlabel", close = "1")
                 push!(new_label, h2(text = location.name))
-                if location.cover != ""
-                    cover = img(src = location.cover, width = 400, align = "center")
+                if location.cover
+                    cover_path = replace(get_filepath(location) * "/images/cover.jpg", " " => "-")
+                    cover = img("activecover", src = cover_path, width = 400, align = "center")
                     style!(cover, "width" => 100percent, "border-radius" => 6px)
                     push!(new_label, cover)
                 end
@@ -210,9 +232,11 @@ main = route("/") do c::Toolips.AbstractConnection
             if ~("$safename-label" in cm)
                 return
             end
+            style!(cm, "activecover", "width" => "auto")
             cm["$safename-label"] = "close" => "0"
             cm["$safename-label"] = "align" => "center"
-            style!(cm, "$safename-label", "left" => 0percent, "width" => 100percent, "top" => 0percent)
+            style!(cm, "$safename-label", "left" => 0percent, "width" => 100percent, "top" => 0percent, 
+                "opacity" => 100percent)
             next!(c, cm, circ) do cm2::ComponentModifier
                 style!(cm2, "$safename-label", "height" => 100percent)
             end
@@ -251,21 +275,6 @@ end
 public_files = mount("/" => "public")
 
 
-abstract type AbstractCustomRoute <: Toolips.AbstractHTTPRoute end
-
-mutable struct CustomRoute <: AbstractCustomRoute
-    path::String
-    page::Function
-end
-                        
-route!(c::AbstractConnection, routes::Routes{AbstractCustomRoute}) = begin
-    target = get_target(c)
-    if contains(target, "@")
-        write!(c, File("user_html/@sampleuser.html"))
-    end
-end
-
-
 # make sure to export!
-export start!, main, default_404, logger, public_files, SESSION
+export start!, main, default_404, logger, public_files, SESSION, LOAD_LOCATIONS
 end # - module ExploreTheCorners <3
